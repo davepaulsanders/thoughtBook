0b1eca4ef7a0dd6841a9767ba8b52aa1ecbc4fdf
Dave Sanders <davepaulsanders@gmail.com>
2022-06-14 12:41:25 -0400

    Populated friends and allowed for adding friends

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 const userController = {
   getAllUsers(req, res) {
     User.find({})
+      .select("-__v")
       .sort({ _id: -1 })
       .then((userData) => res.json(userData))
       .catch((err) => {
@@ -12,6 +13,11 @@ const userController = {
   },
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((userData) => {
         if (!userData) {
           res.sendStatus(404).json({ message: "No user with that id!" });
@@ -56,6 +62,23 @@ const userController = {
       .catch((err) => {
         console.log(err);
         res.sendStatus(400).json(err);
+      });
+  },
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((userData) => {
+        if (!userData) {
+          res.sendStatus(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        res.json(err);
       });
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,8 +6,10 @@ const {
   getUserById,
   updateUser,
   deleteUser,
+  addFriend,
 } = require("../../controllers/user-controller");
 
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:id/friends/:friendId").post(addFriend);
 module.exports = router;
