6cfb66c6f138dcd10cd0214a7f7f649952b2beea
Dave Sanders <davepaulsanders@gmail.com>
2022-06-14 12:24:31 -0400

    Adding routes for User

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,9 +10,49 @@ const userController = {
         res.sendStatus(400).json(err);
       });
   },
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then((userData) => {
+        if (!userData) {
+          res.sendStatus(404).json({ message: "No user with that id!" });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400).json(err);
+      });
+  },
   createUser({ body }, res) {
     User.create(body)
       .then((userData) => res.json(userData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400).json(err);
+      });
+  },
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    }).then((userData) => {
+      if (!userData) {
+        res.sendStatus(404).json({ message: "No user with this id!" });
+        return;
+      }
+      res.json(userData);
+    });
+  },
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((userData) => {
+        if (!userData) {
+          res.sendStatus(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(userData);
+      })
       .catch((err) => {
         console.log(err);
         res.sendStatus(400).json(err);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,8 +3,11 @@ const router = require("express").Router();
 const {
   getAllUsers,
   createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/user-controller");
 
 router.route("/").get(getAllUsers).post(createUser);
-
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 module.exports = router;
